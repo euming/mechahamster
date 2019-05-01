d9191467e2168309412b3ce23254cfb7ae464699
ascmt <achittur@mercenarytechnology.com>
2019-05-01 09:44:03 -0700

    top right status display
    Terribly hacked StateManager to display status in the HUD UI of Lobby, Joining Open Match, and In Open Match status messages in the top right corner.  Done this way to avoid potentially affecting the very fragile State flow control.

--- Assets/Hamster/Scripts/States/StateManager.cs
+++ Assets/Hamster/Scripts/States/StateManager.cs
@@ -12,9 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using customNetwork;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 namespace Hamster.States {
 
@@ -79,9 +82,144 @@ namespace Hamster.States {
       CurrentState().Initialize();
     }
 
+        static bool ClientOpenMatchFoundDetected = false;
+        private static String CalcAutoguiString(BaseState curState)
+        {
+            if (curState is ClientLoadingLevel)
+            {
+                if (CommonData.mainGame.stateManager.CurrentState() is LevelFinished)
+                {
+                    ClientOpenMatchFoundDetected = false;   // reset this jiggery-pokery
+                    return String.Empty;
+                }
+
+                if(ClientOpenMatchFoundDetected)
+                {
+                    return "Go!\r\nIn Open Match";
+                }
+
+                return "Lobby";
+            }
+
+
+            if (curState is ClientOpenMatchStart)
+            {
+                return "Starting Momentarily\r\nJoining Open Match";
+            }
+
+            if(curState is ClientOpenMatchFound)
+            {
+                ClientOpenMatchFoundDetected = true;
+                return "Found Open Match";
+            }
+
+            return "";
+        }
+
+        static Menus.SingleLabelGUI autoGui;
+        static int autoGuiStartCount;
+        static string lastAutoGuiText = String.Empty;
+        private static void UpdateAutogui(BaseState curState)
+        {
+            if (curState != null)// && curState.gui == null)
+            {
+                String str = CalcAutoguiString(curState);
+                if (autoGui == null)
+                {
+                    //Debug.Log("UpdateAutogui: Started " + str + " #" + autoGuiStartCount++.ToString());
+                    autoGui = s_SpawnSimpleLabelUI(str);
+                    if (autoGui == null)
+                    {
+                        return;
+                    }
+                }
+                if (lastAutoGuiText != str)
+                {
+                    Debug.Log("UpdateAutogui: Transitioned from " + lastAutoGuiText + " to " + str);
+                    autoGui.LabelText.text = lastAutoGuiText = str;
+                }
+            }
+            else if (autoGui != null)
+            {
+                lastAutoGuiText = autoGui.LabelText.text = "...";
+            }
+        }
+		
+        private static void s_SetupLabel(ref Menus.SingleLabelGUI menuComponent, string text)
+        {
+            if (menuComponent != null)
+            {
+                menuComponent.LabelText.fontSize /= 2;
+                menuComponent.LabelText.text = text;
+
+                VerticalLayoutGroup vlg = menuComponent.LabelText.GetComponentInParent<VerticalLayoutGroup>();
+                vlg.childAlignment = TextAnchor.UpperLeft;
+
+                Image img = menuComponent.LabelText.GetComponentInParent<Image>();
+                if (img != null)
+                {
+                    img.color = Color.clear;
+                }
+            }
+        }
+
+        private static void s_PositionLabel(ref Menus.SingleLabelGUI menuComponent)
+        {
+            // adapt the resize hackery from Gameplay.PositionButton()
+            GameObject resizeObj = menuComponent.LabelText.transform.parent.gameObject;
+            RectTransform rt = resizeObj.GetComponent<RectTransform>();
+            Vector2 localLowerLeft = rt.anchorMin + rt.offsetMin;
+            Vector2 localUpperRight = rt.anchorMax + rt.offsetMax;
+            Camera camera = CommonData.mainCamera.GetComponentInChildren<Camera>();
+            // Locations of the corners of the button, in screen space.
+            Vector2 screenLowerLeft =
+                camera.WorldToScreenPoint(rt.TransformPoint(localLowerLeft));
+            Vector2 screenUpperRight =
+                camera.WorldToScreenPoint(rt.TransformPoint(localUpperRight));
+
+            Vector2 localDimension = new Vector2(
+                Mathf.Abs(localUpperRight.x - localLowerLeft.x) * 0.5f,
+                Mathf.Abs(localUpperRight.y - localLowerLeft.y));
+
+            float pixelsToLocalUnits = localDimension.x /
+                Mathf.Abs(screenLowerLeft.x - screenUpperRight.x);
+
+            // Move back button to upper right corner
+            resizeObj.transform.localPosition = new Vector3(
+                resizeObj.transform.localPosition.x +
+                (Screen.width * pixelsToLocalUnits - localDimension.x) / 2.0f,
+                resizeObj.transform.localPosition.y +
+                (Screen.height * pixelsToLocalUnits - localDimension.y) / 2.0f,
+                resizeObj.transform.localPosition.z);
+        }
+
+        // Depth to spawn UI, when running in mobile mode.
+        const float UIDepthMobile = 6.0f;
+        // Depth to spawn UI, when running in VR mode.
+        const float UIDepthVR = 10.0f;
+
+        private static Menus.SingleLabelGUI s_SpawnSimpleLabelUI(string text)
+        {
+            if (CommonData.prefabs == null || CommonData.prefabs.menuLookup == null || !CommonData.prefabs.menuLookup.ContainsKey(StringConstants.PrefabsSingleLabelMenu))
+            {
+                Debug.LogWarning("UpdateAutogui s_SpawnSimpleLabelUI: Failed with " + text);
+                return null;
+            }
+            GameObject gui = GameObject.Instantiate(CommonData.prefabs.menuLookup[StringConstants.PrefabsSingleLabelMenu]);
+            gui.transform.position = new Vector3(0, 0, CommonData.inVrMode ? UIDepthVR : UIDepthMobile);
+            gui.transform.SetParent(CommonData.mainCamera.transform, false);
+            Menus.SingleLabelGUI menuComponent = gui.GetComponent< Menus.SingleLabelGUI>();
+            s_SetupLabel(ref menuComponent, text);
+            s_PositionLabel(ref menuComponent);
+            return menuComponent;
+        }
     // Called by the main game every update.
     public void Update() {
       CurrentState().Update();
+      if (MultiplayerGame.instance != null && this == MultiplayerGame.instance.clientStateManager)
+      {
+        UpdateAutogui(CurrentState());
+      }
     }
 
     // Called by the main game every fixed update.
